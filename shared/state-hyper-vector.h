cad15b01912df247ec0349316b4f0134c2e13e1d
Hamid Allaei <hamid@localhost.localdomain>
2015-05-28 17:04:44 +0200

    Improved state hyper vector header file

--- shared/state-hyper-vector.h
+++ shared/state-hyper-vector.h
@@ -4,44 +4,87 @@
 #include "c2dvector.h"
 
 class State_Hyper_Vector{
+	void Init_Random_Generator();
+	~State_Hyper_Vector();
 public:
+	gsl_rng* gsl_r;
 	int N;
 	BasicParticle0* particle;
+	
+	State_Hyper_Vector(int);
+	
+	State_Hyper_Vector& operator= ( const State_Hyper_Vector& sv);
+	State_Hyper_Vector& operator+ (const State_Hyper_Vector& s1) const;
+	State_Hyper_Vector& operator- (const State_Hyper_Vector& s1) const;
+	Real operator* (const State_Hyper_Vector& s1) const;
+
+	void Rand(const Real position_amplitude, const Real angle_amplitude);
 	Real Square() const;
 	Real Magnitude() const;
-	State_Hyper_Vector operator+ (const State_Hyper_Vector& s1) const;
-	State_Hyper_Vector operator- (const State_Hyper_Vector& s1) const;
-	Real operator* (const State_Hyper_Vector& s1) const;
 };
 
-
-State_Hyper_Vector State_Hyper_Vector::operator+ (const State_Hyper_Vector& s1) const
+void State_Hyper_Vector::Init_Random_Generator()
 {
-	State_Hyper_Vector result;
+	const gsl_rng_type * T;
+	gsl_rng_env_setup();
+	gsl_rng_default_seed = 0;
+	T = gsl_rng_default;
+	gsl_r = gsl_rng_alloc (T);
+	gsl_rng_memcpy (gsl_r, C2DVector::gsl_r);
+}
 
+State_Hyper_Vector::State_Hyper_Vector(int particle_number) : N(particle_number)
+{
+	Init_Random_Generator();
+	particle = new BasicParticle0[N];
+}
+
+State_Hyper_Vector::State_Hyper_Vector(const State_Hyper_Vector& sv) : N(sv.N)
+{
+	Init_Random_Generator();
+	gsl_rng_memcpy (gsl_r, sv.gsl_r);
+	particle = new BasicParticle0[N];
+	for (int i = 0; i < N; i++)
+		particle[i] = sv.particle[i];
+}
+
+State_Hyper_Vector::~State_Hyper_Vector()
+{
+	gsl_rng_free(gsl_r);
+	delete [] particle;
+}
+
+State_Hyper_Vector& State_Hyper_Vector::operator= ( const State_Hyper_Vector& sv)
+{
+	gsl_rng_memcpy (gsl_r, sv.gsl_r);
+	for (int i = 0; i < N; i++)
+		particle[i] = sv.particle[i];
+	return *this;
+}
+
+State_Hyper_Vector& State_Hyper_Vector::operator+ (const State_Hyper_Vector& s1) const
+{
 	for (int i = 0; i < N; i++)
 	{
-		result.particle[i].r = (particle[i].r + s1.particle[i].r);
-		result.particle[i].r.Periodic_Transform();
-		result.particle[i].theta = particle[i].theta + s1.particle[i].theta;
-		result.particle[i].theta -= (int) (result.particle[i].theta / (2*M_PI));
+		particle[i].r = (particle[i].r + s1.particle[i].r);
+		particle[i].r.Periodic_Transform();
+		particle[i].theta = particle[i].theta + s1.particle[i].theta;
+		particle[i].theta -= (int) (result.particle[i].theta / (2*M_PI));
 	}
-	return result;
+	return *this;
 }
 
 
 State_Hyper_Vector State_Hyper_Vector::operator- (const State_Hyper_Vector& s1) const
 {
-	State_Hyper_Vector result;
-
 	for (int i = 0; i < N; i++)
 	{
-		result.particle[i].r = (particle[i].r - s1.particle[i].r);
-		result.particle[i].r.Periodic_Transform();
-		result.particle[i].theta = particle[i].theta - s1.particle[i].theta;
-		result.particle[i].theta -= (int) (result.particle[i].theta / (2*M_PI));
+		particle[i].r = (particle[i].r - s1.particle[i].r);
+		particle[i].r.Periodic_Transform();
+		particle[i].theta = particle[i].theta - s1.particle[i].theta;
+		particle[i].theta -= (int) (result.particle[i].theta / (2*M_PI));
 	}
-	return result;
+	return *this;
 }
 
 Real State_Hyper_Vector::operator* (const State_Hyper_Vector& s1) const
@@ -55,6 +98,17 @@ Real State_Hyper_Vector::operator* (const State_Hyper_Vector& s1) const
 	}
 	return result;
 }
+
+void State_Hyper_Vector::Rand(const Real position_amplitude, const Real angle_amplitude)
+{
+	for (int i = 0; i < N; i++)
+	{
+		particle[i].r.x = gsl_ran_flat(gsl_r, -position_amplitude, position_amplitude);
+		particle[i].r.y = gsl_ran_flat(gsl_r, -position_amplitude, position_amplitude);
+		particle[i].theta = gsl_ran_flat(gsl_r, -angle_amplitude, angle_amplitude);;
+	}
+}
+
 
 Real State_Hyper_Vector::Square() const
 {
